94d1b0a3ba6825fd8a94164829a18a439c661bdf
rihamissa123 <riham.issa123@gmail.com>
2023-08-10 11:49:13 +0400

    third change

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 
 
 var _ServicesContainer=document.getElementById("servicesContainer");
-for (let i = 0; i < 5; i++) {
-  let _icon="screen";
-  var content = `
-<div class="col-lg-4">
-<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20">
-  <div class="float-left mar-right20">
-    <a href="#" class="hi-icon hi-icon-${_icon}">screen</a>
-  </div>
-</div>
-<h3 class="text-bold">Responsive ${i}</h3>
-<p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>`;
 
-let _div = document.createElement('div');
-_div.innerHTML = content;
-_div.className= 'col-lg-4 col-lg-4 icon-screen';
-_ServicesContainer.appendChild(_div);
+
+fetch("http://localhost:3000/services")
+.then(response=>response.json())
+.then(json=>{
+  for (let i = 0; i <json.length; i++) {
+    let _icon="screen";
+    var content = `
+  <div class="col-lg-4">
+  <div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20">
+    <div class="float-left mar-right20">
+      <a href="#" class="hi-icon hi-${json[i].icon}">screen</a>
+    </div>
+  </div>
+  <h3 class="text-bold">${json[i].title}</h3>
+  <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>`;
+  
+  let _div = document.createElement('div');
+  _div.innerHTML = content;
+  _div.className= 'col-lg-4 col-lg-4 icon-screen';
+  _ServicesContainer.appendChild(_div);
+
+}})
+
     
-}
